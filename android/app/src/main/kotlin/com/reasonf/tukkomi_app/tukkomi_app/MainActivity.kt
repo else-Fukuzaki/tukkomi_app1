a2c546115cdcebcb25d472c9782a7e187166add8
Yasunori Fukuzaki <i@else.ws>
2025-07-15 13:07:21 +0900

    first_tukkomi1

---
+++ android/app/src/main/kotlin/com/reasonf/tukkomi_app/tukkomi_app/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.reasonf.tukkomi_app.tukkomi_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/screens/home_screen.dart
@@ -0,0 +1,107 @@
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({super.key});
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  // String _text01 = 'おめでとうございます';
+  // String _text02 = '合格です';
+  // String _text03 = '良くできました';
+  // String _text04 = '残念でした';
+  // String _text05 = '不合格です';
+  // String _text06 = '頑張りましょう';
+
+  final List<String> _texts = [
+    'おめでとうございます',
+    '合格です',
+    '良くできました',
+    '残念でした',
+    '不合格です',
+    '頑張りましょう',
+  ];
+
+  final List<String> _soundPaths = [
+    'assets/sounds/sound1.mp3',
+    'assets/sounds/sound2.mp3',
+    'assets/sounds/sound3.mp3',
+    'assets/sounds/sound4.mp3',
+    'assets/sounds/sound5.mp3',
+    'assets/sounds/sound6.mp3',
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text("ツッコミマシーン"), centerTitle: true),
+      body: Container(
+        padding: EdgeInsets.all(8.0),
+        child: Column(
+          children: [
+            Expanded(
+              flex: 1,
+              child: Row(
+                crossAxisAlignment: CrossAxisAlignment.stretch,
+                children: [
+                  Expanded(
+                    flex: 1,
+                    child: _soundButton(_texts[0], _soundPaths[0]),
+                  ),
+                  Expanded(child: _soundButton(_texts[1], _soundPaths[1])),
+                ],
+              ),
+            ),
+            Expanded(
+              flex: 1,
+              child: Row(
+                crossAxisAlignment: CrossAxisAlignment.stretch,
+                children: [
+                  Expanded(
+                    flex: 1,
+                    child: _soundButton(_texts[2], _soundPaths[2]),
+                  ),
+                  Expanded(
+                    flex: 1,
+                    child: _soundButton(_texts[3], _soundPaths[3]),
+                  ),
+                ],
+              ),
+            ),
+            Expanded(
+              flex: 1,
+              child: Row(
+                crossAxisAlignment: CrossAxisAlignment.stretch,
+                children: [
+                  Expanded(
+                    flex: 1,
+                    child: _soundButton(_texts[4], _soundPaths[4]),
+                  ),
+                  Expanded(
+                    flex: 1,
+                    child: _soundButton(_texts[5], _soundPaths[5]),
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget _soundButton(String displayText, String soundPath) {
+    return Container(
+      padding: EdgeInsets.all(8.0),
+      child: ElevatedButton(
+          onPressed: () => _playSound(soundPath),
+          child: Text(displayText)),
+    );
+  }
+
+  void _playSound(String soundPath) {
+    //TODO
+  }
+}
